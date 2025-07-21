2c7d2e00e6578f0a4dee87372f3d6685f708b925
Edmond Wang <mredmondwang@gmail.com>
2025-07-21 10:51:40 -0700

    Fix webpack static configuration error
    - Remove invalid 'index' property from static config in webpack.config.proxy.js
    - npm run dev:proxy now works correctly without webpack schema errors
    - proxy-test.html properly accessible at localhost:3000/proxy-test.html

--- frontends/meshcat/webpack.config.proxy.js
+++ frontends/meshcat/webpack.config.proxy.js
@@ -57,8 +57,7 @@ module.exports = {
   // },
   devServer: {
     static: {
-      directory: path.join(__dirname, './'),
-      index: 'proxy-test.html'
+      directory: path.join(__dirname, './')
     },
     port: 3000,
     hot: true,
